57cb18254cc7941a8daca5dc41da6fe0137ad651
RoberPerez <88011565+RoberPerez@users.noreply.github.com>
2022-02-13 14:16:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ var query = require('samp-query');
 //@audit Settings
 
 const botChar = "/"; // Bot prefix character
-let Samp_IP = "51.178.138.254";
+let Samp_IP = "S1.DIXEL-RP.COM";
 let Samp_Port = 7777;
 let Community_Tag ="WG";
 
